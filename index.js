bc2cf976d554d87392cbc348dcbf4e632102d26d
Volkov Danil <d.volkov@1c-nt.ru>
2018-11-09 13:17:40 +0500

    second commit

--- index.js
+++ index.js
@@ -4,5 +4,24 @@ let ctx = canvas.getContext("2d");
 let x = 0;
 let y = 0;
 
+console.log(canvas.width);
 
+const right = () => {
+    x += 10;
+}
 
+const left = () => {
+    x -= 10;
+}
+
+const draw = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "green";
+    ctx.fillRect(x, y, item, item);
+    if(x+item < canvas.width){
+        right();
+    }else{}
+    console.log(x);
+}
+
+requestAnimationFrame(draw);
